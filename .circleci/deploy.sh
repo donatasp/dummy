43757687d53e04586b5417898788eaf47e3d2664
Donatas Pranckenas <donatas@oberlo.com>
2017-10-25 18:00:47 +0300

    zip -> tar

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-    zip -r ./dist_$CIRCLE_SHA1.zip ./dist
-    aws s3 cp ./dist_$CIRCLE_SHA1.zip s3://oberlo-artifacts/dummy/dist_$CIRCLE_SHA1.zip
+    tar -zcvf ./dist_$CIRCLE_SHA1.tar.gz ./dist
+    aws s3 cp ./dist_$CIRCLE_SHA1.tar.gz s3://oberlo-artifacts/dummy/dist_$CIRCLE_SHA1.tar.gz
     git push heroku master
